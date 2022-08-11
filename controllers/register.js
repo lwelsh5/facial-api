b68886113685dc7b4ecad890026e93fc560faecf
Ellen Welsh <Ellen@Ellens-MacBook-Pro.local>
2022-08-11 20:19:00 +0100

    fix

--- controllers/register.js
+++ controllers/register.js
@@ -26,7 +26,8 @@ const handleRegister = (req, res, db, bcrypt) => {
         .then(trx.commit)
         .catch(trx.rollback)
       })
-      .catch(err => res.status(400).json('err'))
+      .catch(err => res.status(400).json)
+      console.log(err)
   }
   
   module.exports = {
